40e4135d2bc99a5c515aa15cd17f1d5c3b745e36
beanboi7 <sr7ramwashere@protonmail.com>
2021-07-30 11:47:28 +0530

    debugged, testing and gui pending

--- main.go
+++ main.go
@@ -6,23 +6,33 @@ import (
 )
 
 const (
-	height = 32
-	width  = 64
+	height int = 32
+	width  int = 64
 )
 
-var boardState [height][width]int
+var board [height][width]int
 var nextState [height][width]int
+var count int
+var cellState int
+var decider int
+var genCount int
 
 func main() {
-	deadState()
+	initState()
 	randomState()
-	renderBoard()
+	genCount = 0
+	for {
+		renderBoard(board)
+		nextBoardState()
+		genCount += 1
+		nextGen(genCount)
+	}
 }
 
-func deadState() {
+func initState() {
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
-			boardState[i][j] = 0
+			board[i][j] = 0
 		}
 	}
 }
@@ -30,35 +40,215 @@ func randomState() {
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
 			randomInt := rand.Intn(2)
-			boardState[i][j] = randomInt
+			board[i][j] = randomInt
 		}
 	}
 }
 
-func renderBoard() {
+func renderBoard(b [height][width]int) {
 	//assign some colors to the dead and alive cells
 	//print out the board to terminal for now row after row
 	//next scope is to use ebiten and display it via GUI
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
-			fmt.Printf("%d ", boardState[i][j])
+			fmt.Printf("%d ", b[i][j])
 		}
 		fmt.Println()
 	}
 }
 
-func nextBoardState(){
-	initialState := boardState
+// Any live cell with 0 or 1 live neighbors becomes dead, because of underpopulation
+// Any live cell with 2 or 3 live neighbors stays alive, because its neighborhood is just right
+// Any live cell with more than 3 live neighbors becomes dead, because of overpopulation
+// Any dead cell with exactly 3 live neighbors becomes alive, by reproduction
+func nextBoardState() {
+	// initialState := boardState
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
-			cell := initialState[i][j]
-			if cell == 0 && i == 0{
-				//edge cases along the top row, then it has only 5 neighbours as opposed to normal 8 neighbours
-				if j == 0
+			cellState = board[i][j]
+			// fmt.Println("i:", i, "j:", j, "cell state is:", cellState)
+			if (i-1 > -1 && i+1 < height) && (j-1 > -1 && j+1 < width) {
+				decider = normalNeighbours(i, j, board)
+			} else if (i == 0 || i == (height-1)) && (j > 0 && j < width-1) {
+				decider = edgeRows(i, j, board)
+			} else if (j == 0 || j == (width-1)) && (i > 0 && i < height-1) {
+				decider = edgeColumns(i, j, board)
+			} else if (i == 0 && j == 0) || (i == 0 && j == width-1) || (i == height-1 && j == 0) || (i == height-1 && j == width-1) {
+				decider = cornerGrids(i, j, board)
+			}
 
-			} else if cell == 1{
+			if cellState == 1 && decider > 3 {
+				cellState = 0
+			} else if cellState == 1 && decider < 2 {
+				cellState = 0
+			} else if cellState == 1 && (decider == 2 || decider == 3) {
+				cellState = 1
+			} else if cellState == 0 && decider == 3 {
+				cellState = 1
+			}
+			board[i][j] = cellState
+		}
+	}
+}
+func normalNeighbours(i, j int, iState [height][width]int) int {
+	count = 0
+	if iState[i][j-1] == 1 {
+		count += 1
+	}
+	if iState[i][j+1] == 1 {
+		count += 1
+	}
+	if iState[i-1][j-1] == 1 {
+		count += 1
+	}
+	if iState[i-1][j] == 1 {
+		count += 1
+	}
+	if iState[i-1][j+1] == 1 {
+		count += 1
+	}
+	if iState[i+1][j] == 1 {
+		count += 1
+	}
+	if iState[i+1][j-1] == 1 {
+		count += 1
+	}
+	if iState[i+1][j+1] == 1 {
+		count += 1
+	}
+	return count
+}
 
+func edgeColumns(i, j int, iState [height][width]int) int {
+	count = 0
+	switch j {
+	case 0:
+		if iState[i-1][j] == 1 {
+			count += 1
+		}
+		if iState[i+1][j] == 1 {
+			count += 1
+		}
+		if iState[i][j+1] == 1 {
+			count += 1
+		}
+		if iState[i-1][j+1] == 1 {
+			count += 1
+		}
+		if iState[i+1][j+1] == 1 {
+			count += 1
+		}
+	case width - 1:
+		if iState[i-1][j] == 1 {
+			count += 1
+		}
+		if iState[i+1][j] == 1 {
+			count += 1
+		}
+		if iState[i][j-1] == 1 {
+			count += 1
+		}
+		if iState[i-1][j-1] == 1 {
+			count += 1
+		}
+		if iState[i+1][j-1] == 1 {
+			count += 1
+		}
+	}
+	return count
+}
+func edgeRows(i, j int, iState [height][width]int) int {
+	count = 0
+	switch i {
+	case 0:
+		if iState[i+1][j] == 1 {
+			count += 1
+		}
+		if iState[i+1][j+1] == 1 {
+			count += 1
+		}
+		if iState[i+1][j-1] == 1 {
+			count += 1
+		}
+		if iState[i][j+1] == 1 {
+			count += 1
+		}
+		if iState[i][j-1] == 1 {
+			count += 1
+		}
+	case height - 1:
+		if iState[i-1][j] == 1 {
+			count += 1
+		}
+		if iState[i-1][j+1] == 1 {
+			count += 1
+		}
+		if iState[i-1][j-1] == 1 {
+			count += 1
+		}
+		if iState[i][j-1] == 1 {
+			count += 1
+		}
+		if iState[i][j+1] == 1 {
+			count += 1
+		}
+	}
+	return count
+}
+
+func cornerGrids(i, j int, iState [height][width]int) int {
+	count = 0
+	switch i {
+	case 0:
+		if j == 0 {
+			if iState[i][j+1] == 1 {
+				count += 1
+			}
+			if iState[i+1][j] == 1 {
+				count += 1
+			}
+			if iState[i+1][j+1] == 1 {
+				count += 1
+			}
+		} else if j == (width - 1) {
+			if iState[i][j-1] == 1 {
+				count += 1
+			}
+			if iState[i+1][j-1] == 1 {
+				count += 1
+			}
+			if iState[i+1][j] == 1 {
+				count += 1
+			}
+		}
+	case height - 1:
+		if j == 0 {
+			if iState[i-1][j] == 1 {
+				count += 1
+			}
+			if iState[i-1][j+1] == 1 {
+				count += 1
+			}
+			if iState[i][j+1] == 1 {
+				count += 1
+			}
+		} else if j == width-1 {
+			if iState[i-1][j] == 1 {
+				count += 1
+			}
+			if iState[i-1][j-1] == 1 {
+				count += 1
+			}
+			if iState[i][j-1] == 1 {
+				count += 1
 			}
 		}
 	}
+	return count
+}
+
+func nextGen(int) {
+	fmt.Println("-----------------------------------------------")
+	fmt.Println("Life's generation:", genCount)
+	fmt.Println("-----------------------------------------------")
 }
